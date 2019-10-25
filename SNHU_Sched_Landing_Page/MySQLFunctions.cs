b5eb769a407cd10285bd13364c4585336ffd5b78
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2019-10-25 13:03:12 -0400

    Update MySQLFunctions.cs

--- SNHU_Sched_Landing_Page/MySQLFunctions.cs
+++ SNHU_Sched_Landing_Page/MySQLFunctions.cs
@@ -11,7 +11,7 @@ namespace SNHU_Sched_Landing_Page
 {
 	public static class MySQLFunctions
     {
-        public const string MYSQLPassword = "beach02";
+        public const string MYSQLPassword = "password";
 
 		public static void SQLCommand(string command)
 		{
